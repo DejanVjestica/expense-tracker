bf62accc0e8cc5140171dff150c5e7953983bebf
dejan.vjestica <dejan.vjestica@aperto.com>
2021-11-29 15:22:44 +0100

    Add typescript and sass

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import "./components/ExpenseItem";
+import ExpenseItem from "./components/ExpenseItem";
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">logo</header>
+
+      <ExpenseItem />
+    </div>
+  );
+}
+
+export default App;
