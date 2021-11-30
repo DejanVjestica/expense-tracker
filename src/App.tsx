bf2b8519d2c066e1ed799b07824a7893916d1f03
dejan.vjestica <dejan.vjestica@aperto.com>
2021-11-30 15:45:45 +0100

    Set eslint and prettier, format the files

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
-import "./App.scss";
-import "./components/ExpenseItem";
-import ExpenseItem from "./components/ExpenseItem";
-import React from "react";
+import React from 'react'
+import ExpenseItem from './components/ExpenseItem'
+import './App.scss'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">logo</header>
-
-      <ExpenseItem />
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">logo</header>
+			<ExpenseItem />
+		</div>
+	)
 }
 
-export default App;
+export default App
