cf395e44c62c2f0c90b28df0ab61e053dec5e2b8
dejan.vjestica <dejan.vjestica@aperto.com>
2022-04-05 07:33:41 +0200

    Clean up App

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import { Expenses } from './components/Expenses'
+import { NewExpense } from './components/newExpense/NewExpense'
+import { Wrapper } from './components/core/Wrapper'
 import './App.scss'
 
 function App() {
 	return (
-		<div className="App">
+		<Wrapper className="App">
 			<header className="App-header">logo</header>
-			<Expenses />
-		</div>
+			<Wrapper className="App-wrapper">
+				<NewExpense />
+				<Expenses />
+			</Wrapper>
+		</Wrapper>
 	)
 }
 
